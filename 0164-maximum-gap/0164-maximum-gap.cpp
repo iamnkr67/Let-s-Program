31b8f69e33b2531ec3c42fe78c1c764ee9cfd183
Nitesh Kumar <niteshkumaro7@hotmail.com>
2025-04-06 01:15:31 +0530

    Time: 15 ms (84.98%), Space: 97.8 MB (14.29%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -4,12 +4,32 @@ public:
         int n = nums.size();
         if(n < 2) return 0;
 
-        sort(nums.begin(), nums.end());
+        int minVal = *min_element(nums.begin(), nums.end());
+        int maxVal = *max_element(nums.begin(), nums.end());
 
-        int maxGap = 0;
+        if(minVal == maxVal) return 0;
 
-        for(int i = 1; i < n; i++) {
-            maxGap = max(maxGap, nums[i] - nums[i-1]);
+        int bucketSize = max(1, (maxVal-minVal)/ (n-1));
+        int bucketCount = (maxVal - minVal) / bucketSize + 1;
+
+        vector<int> bucketMin(bucketCount, INT_MAX);
+        vector<int> bucketMax(bucketCount, INT_MIN);
+        vector<int> bucketUsed(bucketCount, false);
+
+        for(int num : nums) {
+            int idx = (num - minVal) / bucketSize;
+            bucketMin[idx] = min(bucketMin[idx], num);
+            bucketMax[idx] = max(bucketMax[idx], num);
+            bucketUsed[idx] = true;
+        }
+
+        int maxGap  = 0;
+        int prevMax = minVal;
+
+        for(int i = 0; i<bucketCount; ++i) {
+            if(!bucketUsed[i]) continue;
+            maxGap = max(maxGap, bucketMin[i] - prevMax);
+            prevMax = bucketMax[i];
         }
 
         return maxGap;
@@ -17,3 +37,9 @@ public:
 };
 
 
+// Finding min and max : O(n)
+// Placing elements into buckets: O(n)
+// Scanning through buckets: O(n)
+// Total: O(n)
+
+// SC: O(n)
